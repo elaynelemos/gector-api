567b7e2acf038c66e253c4171f6921b1e424b83d
elayne.lemos <elayne.lemos@sysvale.com>
2022-10-31 21:45:13 -0300

    feat: initial app structure

--- gector_api/app.py
+++ gector_api/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!</p>'
+
+@app.route('/correct')
+def correct_sentence():
+    return request.get_json()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
