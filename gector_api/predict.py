0aab65e88b74b9d5f4b18ef7c0c430777f29b86d
elaynelemos <elayne.l.lemos@gmail.com>
2022-11-07 20:54:10 -0300

    fix: return

--- gector_api/predict.py
+++ gector_api/predict.py
@@ -46,29 +46,6 @@ def correct_sentences(sentences, vocab_path, model_path, max_len, min_len, itera
                          is_ensemble=is_ensemble,
                          weigths=weights)
 
-    cnt_corrections = predict_for_sentences(sentences, model,
-                                            batch_size=batch_size,
-                                            to_normalize=normalize)
-    # evaluate with m2 or ERRANT
-    print(f"Produced overall corrections: {cnt_corrections}")
-
-
-# sentences = f'Nevertheless , while I fell that a carrier should not be obligated to tell his or her relatives about the genetic risk , I would like to suggest the person to at least share the information with her or his potential partner ( husband or wife ) since the individual \'s genetic risk may affect their future children .\n'
-# correct_sentences(
-#     sentences=args.sentences,
-#     vocab_path=args.vocab_path,
-#     model_path=args.model_path,
-#     max_len=args.max_len,
-#     min_len=args.min_len,
-#     iteration_count=args.iteration_count,
-#     min_error_probability=args.min_error_probability,
-#     lowercase_tokens=args.lowercase_tokens,
-#     transformer_model=args.transformer_model,
-#     special_tokens_fix=args.special_tokens_fix,
-#     additional_confidence=args.additional_confidence,
-#     batch_size=args.batch_size,
-#     additional_del_confidence=args.additional_del_confidence,
-#     is_ensemble=args.is_ensemble,
-#     normalize=args.normalize,
-#     weights=args.weights
-# )
+    return predict_for_sentences(sentences, model,
+                                batch_size=batch_size,
+                                to_normalize=normalize)
