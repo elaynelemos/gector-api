2066337d5a6f73fb144377252eb9c8146ab23a7d
elaynelemos <elayne.l.lemos@gmail.com>
2022-11-20 17:33:55 -0300

    fix: load model only once

--- gector_api/app.py
+++ gector_api/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from gector_api.predict import correct_sentences
-import os
 
 app = Flask(__name__)
 
@@ -10,27 +9,13 @@ def hello_world():
 
 @app.route('/correct', methods=['POST'])
 def correct_sentence():
-    working_dir = os.getcwd()
     request_json = request.get_json()
     sentence = request_json.get('sentence')
 
     corrected = correct_sentences(
         sentences=sentence,
-        vocab_path=f'{working_dir}/data/output_vocabulary/',
-        model_path=f'{working_dir}/models/xlnet_0_gectorv2.th',
-        max_len=200,
-        min_len=3,
-        iteration_count=5,
-        min_error_probability=0.0,
-        lowercase_tokens=0,
-        transformer_model='xlnet',
-        special_tokens_fix=0,
-        additional_confidence=0,
         batch_size=128,
-        additional_del_confidence=0,
-        is_ensemble=0,
-        normalize='store_true',
-        weights=None
+        normalize=None,
     )
     return {
         'corrected':corrected
--- gector_api/predict.py
+++ gector_api/predict.py
@@ -1,5 +1,39 @@
 from utils.helpers import normalize
 from gector.gec_model import GecBERTModel
+from os import getcwd
+
+working_dir = getcwd()
+
+vocab_path=f'{working_dir}/data/output_vocabulary/'
+model_path=f'{working_dir}/models/xlnet_0_gectorv2.th'
+max_len=200
+min_len=3
+iteration_count=5
+min_error_probability=0.0
+lowercase_tokens=0
+transformer_model='xlnet'
+special_tokens_fix=0
+additional_confidence=0
+additional_del_confidence=0
+is_ensemble=0
+weights=None
+
+
+model = GecBERTModel(
+    vocab_path=vocab_path,
+    model_paths=model_path,
+    max_len=max_len, min_len=min_len,
+    iterations=iteration_count,
+    min_error_probability=min_error_probability,
+    lowercase_tokens=lowercase_tokens,
+    model_name=transformer_model,
+    special_tokens_fix=special_tokens_fix,
+    log=False,
+    confidence=additional_confidence,
+    del_confidence=additional_del_confidence,
+    is_ensemble=is_ensemble,
+    weigths=weights
+)
 
 
 def predict_for_sentences(sentences, model, batch_size=32, to_normalize=False):
@@ -27,25 +61,10 @@ def predict_for_sentences(sentences, model, batch_size=32, to_normalize=False):
     return [ "\n".join(result_lines), cnt_corrections ]
 
 
-def correct_sentences(sentences, vocab_path, model_path, max_len, min_len, iteration_count,
-                        min_error_probability, lowercase_tokens, transformer_model,
-                        special_tokens_fix, additional_confidence, batch_size,
-                        additional_del_confidence, is_ensemble, normalize, weights):
-    # get all paths
-    model = GecBERTModel(vocab_path=vocab_path,
-                         model_paths=model_path,
-                         max_len=max_len, min_len=min_len,
-                         iterations=iteration_count,
-                         min_error_probability=min_error_probability,
-                         lowercase_tokens=lowercase_tokens,
-                         model_name=transformer_model,
-                         special_tokens_fix=special_tokens_fix,
-                         log=False,
-                         confidence=additional_confidence,
-                         del_confidence=additional_del_confidence,
-                         is_ensemble=is_ensemble,
-                         weigths=weights)
+def correct_sentences(sentences, batch_size, normalize):
+    global model
 
-    return predict_for_sentences(sentences, model,
+    return predict_for_sentences(sentences,
+                                model=model,
                                 batch_size=batch_size,
                                 to_normalize=normalize)
